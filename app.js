42898802a8e3056c454b8441b68acf97c8dce675
Usman <usmanofficial943@gmail.com>
2024-04-06 08:25:20 +0500

    modules creation

---
+++ app.js
@@ -0,0 +1,6 @@
+import slogin from './student.js';
+import tlogin from './teacher.js';
+let teacher = new tlogin();
+console.log(teacher.data);
+let student = new slogin();
+console.log(student.data);
---
+++ app.ts
@@ -0,0 +1,8 @@
+import slogin from './student.js';
+import tlogin from './teacher.js';
+
+let teacher= new tlogin();
+console.log(teacher.data)
+
+let student=new slogin();
+console.log(student.data)
